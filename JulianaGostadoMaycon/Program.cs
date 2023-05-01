50d28df49bfe351facd1004073353d30790cdc93
Maycon Silva <mayconsilva_cp@icloud.com>
2023-05-01 19:35:48 -0300

    Adicionar arquivos de projeto.

---
+++ JulianaGostadoMaycon/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace JulianaGostadoMaycon
+{
+    public class Program
+    {
+        public static int  idadeJu ;
+        public static string nomeJu;
+        public static double idadeJu2;
+        public static float valor;
+        static void Main(string[] args)
+        {
+            
+
+            nomeJu = Console.ReadLine();
+
+           // Console.WriteLine("O nome da pessoa informada é "+ nomeJu);
+
+            //Console.WriteLine("Informe a idade da pessoa ");
+
+            //idadeJu = Convert.ToInt32(Console.ReadLine());
+
+            //.WriteLine("A idade que voce informou é " + idadeJu);
+
+
+            Console.WriteLine("Pergunta a idade novamente ");
+            idadeJu2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Por favor, informe o nome da pessoa!");
+            Console.WriteLine("A idade que você informou é " + idadeJu2);
+
+           
+        }
+    }
+}
+
+
